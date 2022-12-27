f30c303012fcf51dd27e9a3b3eaf46eb054021ff
Sabry Awad <dr.sabry1997@gmail.com>
2022-12-27 07:04:25 +0200

    using hashset avoid match use if statement

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,49 @@
+// pub fn split_string_on_uppercase_chars(s: &str, special_chars: &[char]) -> Vec<String> {
+//     let mut result = Vec::new();
+//     let mut current_word = String::new();
+
+//     let mut prev_char: Option<char> = None;
+//     for c in s.chars() {
+//         match (prev_char, c) {
+//             (Some(p), c) if c.is_uppercase() => {
+//                 if !(p.is_uppercase() || p.is_whitespace() || special_chars.contains(&p)) {
+//                     if !current_word.is_empty() {
+//                         result.push(current_word);
+//                     }
+//                     current_word = String::new();
+//                 }
+//                 current_word.push(c);
+//             }
+//             _ => current_word.push(c),
+//         }
+//         prev_char = Some(c);
+//     }
+
+//     if !current_word.is_empty() {
+//         result.push(current_word);
+//     }
+
+//     result
+// }
+
+use std::collections::HashSet;
+
 pub fn split_string_on_uppercase_chars(s: &str, special_chars: &[char]) -> Vec<String> {
-    let mut result = Vec::new();
-    let mut current_word = String::new();
+    let mut result = Vec::with_capacity(s.len());
+    let mut current_word = String::with_capacity(s.len());
 
     let mut prev_char: Option<char> = None;
+    let special_chars_set: HashSet<char> = special_chars.iter().copied().collect();
     for c in s.chars() {
-        match (prev_char, c) {
-            (Some(p), c) if c.is_uppercase() => {
-                if !(p.is_uppercase() || p.is_whitespace() || special_chars.contains(&p)) {
-                    if !current_word.is_empty() {
-                        result.push(current_word);
-                    }
+        if let Some(p) = prev_char {
+            if c.is_uppercase() && !(p.is_uppercase() || p.is_whitespace() || special_chars_set.contains(&p)) {
+                if !current_word.is_empty() {
+                    result.push(current_word);
                     current_word = String::new();
                 }
-                current_word.push(c);
             }
-            _ => current_word.push(c),
         }
+        current_word.push(c);
         prev_char = Some(c);
     }
 
@@ -25,4 +53,3 @@ pub fn split_string_on_uppercase_chars(s: &str, special_chars: &[char]) -> Vec<S
 
     result
 }
-
